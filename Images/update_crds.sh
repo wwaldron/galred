45a8cd37dccb8c547e2dd6596fd47e4d108477cf
Will Waldron <wwaldron@harding.edu>
2023-07-21 16:29:21 -0500

    Added CRDS updater

---
+++ Images/update_crds.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# Updates my local CRDS repo for the current image set
+# Must be run in conda stenv
+
+# Update the References
+# This assumes that the CRDS_SERVER_URL & CRDS_PATH environment variables have been set
+crds bestrefs --files mastDownload/HST/*/*flc.fits --sync-references=1 --update-bestrefs
