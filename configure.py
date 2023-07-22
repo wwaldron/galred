beb26eca19a919b9b89841b90a40bc5e84dd9532
Will Waldron <wwaldron@harding.edu>
2023-07-22 10:44:26 -0500

    Began writing the configuration file

---
+++ configure.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""This is the configuration file to be used when this template is used to
+process a repository.
+
+Examples
+--------
+
+`$ ./configure "ESO 137-001"`
+
+"""
+
+# Python Imports
+from glob import iglob
+from argparse import ArgumentParser
+
+
+# --- Argument Parsing --- #
+# Create the Parser
+prsr = ArgumentParser(
+    description=__doc__
+)
+
+# Add Arguments
+prsr.add_argument(
+    'title', type=str,
+    help='Galaxy title to utilize'
+)
+
+# Get the Arguments
+args = prsr.parse_args()
+
+
+# --- Replace Strings --- #
+# Prepare the Title
+galTitle  = args.title.strip().upper()
+galTitle  = ' '.join(galTitle.split())
+shrtTitle = ''.join(galTitle.split())
+dashTitle = '-'.join(galTitle.split())
+
+# Loop through Notebooks
+for fn in iglob('**/*.ipynb', recursive=True):
+
+    # Open File for Writing
+    with open(fn) as fid:
+
+        # Get Lines
+        fileText = fid.read()
+
+        # Replace Text
+        fileText = fileText.replace('[GALAXY_TITLE]', galTitle)
+        fileText = fileText.replace('[GALAXY_SHORT_TITLE]', shrtTitle)
+        fileText = fileText.replace('[GALAXY_DASH_TITLE]', dashTitle)
